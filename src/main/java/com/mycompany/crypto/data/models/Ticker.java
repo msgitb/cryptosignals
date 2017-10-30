cc0be72bc4b174fce912dd2f31820cfbe5beed4d
msgitb <omarcelito@gmail.com>
2017-10-30 17:44:17 +0000

    Added Ticker class to track tickerdata

---
+++ src/main/java/com/mycompany/crypto/data/models/Ticker.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.crypto.data.models;
+
+import com.google.gson.Gson;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author marcelo
+ */
+public class Ticker {
+    
+    public final BigDecimal last;
+    public final BigDecimal lowestAsk;
+    public final BigDecimal highestBid;
+    public final BigDecimal percentChange;
+    public final BigDecimal baseVolume;
+    public final BigDecimal quoteVolume;
+
+    public Ticker(BigDecimal last, BigDecimal lowestAsk, BigDecimal highestBid, BigDecimal percentChange, BigDecimal baseVolume, BigDecimal quoteVolume)
+    {
+        this.last = last;
+        this.lowestAsk = lowestAsk;
+        this.highestBid = highestBid;
+        this.percentChange = percentChange;
+        this.baseVolume = baseVolume;
+        this.quoteVolume = quoteVolume;
+    }
+
+    @Override
+    public String toString()
+    {
+        return new Gson().toJson(this);
+    }
+}
